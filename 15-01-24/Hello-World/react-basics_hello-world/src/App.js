0feeb50ecbbbdb114a32d9f1e56ba0e0ef6326f1
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-15 11:13:45 +0100

    react-basics_hello-world doneA

---
+++ 15-01-24/Hello-World/react-basics_hello-world/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+
+function HelloWorld() {
+  return <h1>Hello world! Eshed</h1>;
+}
+
+export default function App() {
+  return HelloWorld();
+}
